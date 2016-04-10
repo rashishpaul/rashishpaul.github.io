d64f7a96c514bffb584ba2075a77a29244533848
Rashish Paul <rashishpaul@gmail.com>
2016-04-10 05:55:47 -0400

    p2p

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
 
   // setup var values
-  var crescents = ["green", "red", "blue", "yellow"];
+  var colors= ["green", "red", "blue", "yellow"];
   var red = [0];
   var blue = [1];
   var green = [2];
   var yellow = [3];
-  var numberOfTurns = 1;
+  var numberOfTurns = "1";
 
 
 //this function generates random sequence(rndmlghts) and sets the length of the round
 var computer = [];
 
-  var rndmlghts = function(roundLength){
-    for (var i = 0; i < roundLength; i++) {
-      computer.push(crescents[Math.floor((Math.random() * crescents.length))]);
-    }
+  var rndmlghts = function(){
+    // for (var i = 0; i < roundLength; i++) {
+      computer.push(colors[Math.floor((Math.random() * colors.length))]);
+    // }
     // return computer;
   };
   //this calls the function to run
@@ -45,8 +45,8 @@ var computer = [];
         var result = []
          computer.forEach(function(value, index){
         if (value === moves[index]){
-         // console.log('win');
-          result.push('win');
+         rndmlghts;
+          // result.push('win');
         } else {
           //console.log('nope');
           result.push('nope');
@@ -56,7 +56,7 @@ var computer = [];
        if (result.indexOf('nope')>-1){
        console.log('lose');
       }else {
-       console.log('win');
+       // console.log('win');
      }
 
 
