7e11b4e89d3ebe8aa396ed71a16f517c4e7d1682
Rashish Paul <rashishpaul@gmail.com>
2016-04-11 09:21:49 -0400

    p2p

--- script.js
+++ script.js
@@ -8,66 +8,60 @@ $(document).ready(function(){
   var yellow = [3];
   var computer = [];
 
-
-  var randomColors = function light(){
-      for(i = 0; i < computer.length; i++);
-      computer.push(Math.floor(Math.random() * colors.length)+1);
-      console.log(computer);
-
-
-  var auto = function gameStart(){
-  if (computer[0] === "green") {
-     $(".green").toggleClass('change');
-  } else if (computer[1] === "red") {
-     $(".red").toggleClass('change');
-  } else if (computer[2] === "blue") {
-    $(".blue").toggleClass('change');
-  } else if (computer[3] === "yellow") {
-    $(".yellow").toggleClass('change');
-  }
-  setTimeout(auto,3000);
-  console.log(computer)
-
-
-//setting up players move inputs with arrow keys
+  //setting up players move inputs with arrow keys
   var moves = [];
   var upkey = '38';
   var rightkey = '39';
   var downkey =  '40';
   var leftkey = '37';
+
+  //computer pick color.
+  var randomColors = function(){
+    for(i = 0; i < computer.length; i++);
+    var rand = Math.floor(Math.random() * colors.length)
+    computer.push(rand);
+    console.log(computer)
+    if (rand == 0) {
+       $(".green").toggleClass('change');
+    } else if (rand == 1) {
+       $(".red").toggleClass('change');
+    } else if (rand == 2) {
+      $(".blue").toggleClass('change');
+    } else if (rand == 3) {
+      $(".yellow").toggleClass('change');
+    }
+  }
+  randomColors(); //call to make cpu pick color.
+
   //used @keyframes animation to get the arrow keys to transition opacity
-  var player = $(document).keydown(function(e) {
-         if (moves.length < computer.length) {
-              if (e.keyCode == leftkey) {
-                  $(".yellow").toggleClass('change'); moves.push("yellow"); console.log(moves);
-              } else if (e.keyCode == rightkey) {
-                  $(".red").toggleClass('change'); moves.push("red"); console.log(moves);
-              } else if (e.keyCode == upkey) {
-                  $(".green").toggleClass('change'); moves.push("green"); console.log(moves);
-              } else if (e.keyCode == downkey) {
-                  $(".blue").toggleClass('change'); moves.push("blue"); console.log(moves);
-                }
-  function winner(){
-         var result = [];
-         if (moves.length === computer.length) {
-         computer.forEach(function(value, index){
-         if (value !== moves[index]){
-           result.indexOf('nope')>-1;
-           console.log('lose');
-         } else {
-         for (var i = 0; i < computer; i++){
-            light();
-        console.log('win');
-       } //closes for loop function winner
+  $(document).keydown(function(e) {
+    if (moves.length < computer.length) {
+      if (e.keyCode == leftkey) {
+          $(".yellow").toggleClass('change'); moves.push("yellow"); console.log(moves);
+      } else if (e.keyCode == rightkey) {
+          $(".red").toggleClass('change'); moves.push("red"); console.log(moves);
+      } else if (e.keyCode == upkey) {
+          $(".green").toggleClass('change'); moves.push("green"); console.log(moves);
+      } else if (e.keyCode == downkey) {
+          $(".blue").toggleClass('change'); moves.push("blue"); console.log(moves);
+      }
+    }
+    //debugger
+    // if computer.last !== moves.last
+      //game over, reset variables, recall randomcolor()
+      if (computer.last !== moves.last){
+        randomColors();alert("you lose")
+      }
 
-      }//closes else cond. function winner
-     })//closes for.Each method in function winner
-  }//closes function winner if statement
+    //if computer.last === movest.last
+      //increment a counter (add up a highscore)
+      //recall randomColors() - do not reset variables.
+      if (computer.last === moves.last){
+        randomColors();
+      }
 
-}//closes funcion winner
-}//closes var player .keydown
-})//closes var player
-}
-}
-})
+  })//closes keydown
+
+
+})//closes onReady
 
